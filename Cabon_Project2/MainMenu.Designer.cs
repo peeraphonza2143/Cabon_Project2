f0d1413d7190ecac5bb07b4d8708b82b43d76c82
peeraphonza2143 <peeraphonza2143@gmail.com>
2024-12-07 19:40:02 +0700

    Navigation Add Active Button

---
+++ Cabon_Project2/MainMenu.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Cabon_Project2
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.Page_6 = new FontAwesome.Sharp.IconButton();
+            this.Page_5 = new FontAwesome.Sharp.IconButton();
+            this.Page_4 = new FontAwesome.Sharp.IconButton();
+            this.Page_3 = new FontAwesome.Sharp.IconButton();
+            this.Page_2 = new FontAwesome.Sharp.IconButton();
+            this.Page_Home = new FontAwesome.Sharp.IconButton();
+            this.PanelLOGO = new System.Windows.Forms.Panel();
+            this.PanelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelMenu
+            // 
+            this.PanelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.PanelMenu.Controls.Add(this.Page_6);
+            this.PanelMenu.Controls.Add(this.Page_5);
+            this.PanelMenu.Controls.Add(this.Page_4);
+            this.PanelMenu.Controls.Add(this.Page_3);
+            this.PanelMenu.Controls.Add(this.Page_2);
+            this.PanelMenu.Controls.Add(this.Page_Home);
+            this.PanelMenu.Controls.Add(this.PanelLOGO);
+            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenu.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(220, 631);
+            this.PanelMenu.TabIndex = 0;
+            // 
+            // Page_6
+            // 
+            this.Page_6.FlatAppearance.BorderSize = 0;
+            this.Page_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Page_6.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Page_6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Page_6.IconChar = FontAwesome.Sharp.IconChar.Cube;
+            this.Page_6.IconColor = System.Drawing.Color.White;
+            this.Page_6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Page_6.IconSize = 32;
+            this.Page_6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_6.Location = new System.Drawing.Point(3, 444);
+            this.Page_6.Name = "Page_6";
+            this.Page_6.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Page_6.Size = new System.Drawing.Size(214, 55);
+            this.Page_6.TabIndex = 6;
+            this.Page_6.Text = "Page 6";
+            this.Page_6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Page_6.UseVisualStyleBackColor = true;
+            this.Page_6.Click += new System.EventHandler(this.Page_6_Click);
+            // 
+            // Page_5
+            // 
+            this.Page_5.FlatAppearance.BorderSize = 0;
+            this.Page_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Page_5.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Page_5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Page_5.IconChar = FontAwesome.Sharp.IconChar.Cube;
+            this.Page_5.IconColor = System.Drawing.Color.White;
+            this.Page_5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Page_5.IconSize = 32;
+            this.Page_5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_5.Location = new System.Drawing.Point(3, 383);
+            this.Page_5.Name = "Page_5";
+            this.Page_5.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Page_5.Size = new System.Drawing.Size(214, 55);
+            this.Page_5.TabIndex = 5;
+            this.Page_5.Text = "Page 5";
+            this.Page_5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Page_5.UseVisualStyleBackColor = true;
+            this.Page_5.Click += new System.EventHandler(this.Page_5_Click);
+            // 
+            // Page_4
+            // 
+            this.Page_4.FlatAppearance.BorderSize = 0;
+            this.Page_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Page_4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Page_4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Page_4.IconChar = FontAwesome.Sharp.IconChar.Cube;
+            this.Page_4.IconColor = System.Drawing.Color.White;
+            this.Page_4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Page_4.IconSize = 32;
+            this.Page_4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_4.Location = new System.Drawing.Point(3, 322);
+            this.Page_4.Name = "Page_4";
+            this.Page_4.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Page_4.Size = new System.Drawing.Size(214, 55);
+            this.Page_4.TabIndex = 4;
+            this.Page_4.Text = "Page 4";
+            this.Page_4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Page_4.UseVisualStyleBackColor = true;
+            this.Page_4.Click += new System.EventHandler(this.Page_4_Click);
+            // 
+            // Page_3
+            // 
+            this.Page_3.FlatAppearance.BorderSize = 0;
+            this.Page_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Page_3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Page_3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Page_3.IconChar = FontAwesome.Sharp.IconChar.Cube;
+            this.Page_3.IconColor = System.Drawing.Color.White;
+            this.Page_3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Page_3.IconSize = 32;
+            this.Page_3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_3.Location = new System.Drawing.Point(3, 261);
+            this.Page_3.Name = "Page_3";
+            this.Page_3.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Page_3.Size = new System.Drawing.Size(214, 55);
+            this.Page_3.TabIndex = 3;
+            this.Page_3.Text = "Page 3";
+            this.Page_3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Page_3.UseVisualStyleBackColor = true;
+            this.Page_3.Click += new System.EventHandler(this.Page_3_Click);
+            // 
+            // Page_2
+            // 
+            this.Page_2.FlatAppearance.BorderSize = 0;
+            this.Page_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Page_2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Page_2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Page_2.IconChar = FontAwesome.Sharp.IconChar.Cube;
+            this.Page_2.IconColor = System.Drawing.Color.White;
+            this.Page_2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Page_2.IconSize = 32;
+            this.Page_2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_2.Location = new System.Drawing.Point(3, 200);
+            this.Page_2.Name = "Page_2";
+            this.Page_2.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Page_2.Size = new System.Drawing.Size(214, 55);
+            this.Page_2.TabIndex = 2;
+            this.Page_2.Text = "Page 2";
+            this.Page_2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Page_2.UseVisualStyleBackColor = true;
+            this.Page_2.Click += new System.EventHandler(this.Page_2_Click);
+            // 
+            // Page_Home
+            // 
+            this.Page_Home.FlatAppearance.BorderSize = 0;
+            this.Page_Home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Page_Home.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Page_Home.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Page_Home.IconChar = FontAwesome.Sharp.IconChar.Cube;
+            this.Page_Home.IconColor = System.Drawing.Color.White;
+            this.Page_Home.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Page_Home.IconSize = 32;
+            this.Page_Home.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_Home.Location = new System.Drawing.Point(3, 139);
+            this.Page_Home.Name = "Page_Home";
+            this.Page_Home.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Page_Home.Size = new System.Drawing.Size(214, 55);
+            this.Page_Home.TabIndex = 1;
+            this.Page_Home.Text = "Home";
+            this.Page_Home.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Page_Home.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Page_Home.UseVisualStyleBackColor = true;
+            this.Page_Home.Click += new System.EventHandler(this.Page_Home_Click);
+            // 
+            // PanelLOGO
+            // 
+            this.PanelLOGO.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelLOGO.Location = new System.Drawing.Point(0, 0);
+            this.PanelLOGO.Name = "PanelLOGO";
+            this.PanelLOGO.Size = new System.Drawing.Size(220, 119);
+            this.PanelLOGO.TabIndex = 0;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 631);
+            this.Controls.Add(this.PanelMenu);
+            this.Font = new System.Drawing.Font("Britannic Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.PanelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelMenu;
+        private FontAwesome.Sharp.IconButton Page_Home;
+        private System.Windows.Forms.Panel PanelLOGO;
+        private FontAwesome.Sharp.IconButton Page_6;
+        private FontAwesome.Sharp.IconButton Page_5;
+        private FontAwesome.Sharp.IconButton Page_4;
+        private FontAwesome.Sharp.IconButton Page_3;
+        private FontAwesome.Sharp.IconButton Page_2;
+    }
+}
+
---
+++ Cabon_Project2/MainMenu.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace Cabon_Project2
+{
+    public partial class MainMenu : Form
+    {
+        private IconButton currentbtn;
+        private Panel leftBorderBtn;
+
+        public MainMenu()
+        {
+            InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7,60);
+            PanelMenu.Controls.Add(leftBorderBtn);
+
+        }
+
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172,126,241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 251);
+        }
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            Console.WriteLine(currentbtn);
+           
+            if (senderBtn != null)
+            {
+                DisableButton();
+
+                currentbtn = (IconButton)senderBtn;
+                currentbtn.BackColor = Color.FromArgb(37,36,81);
+                currentbtn.ForeColor = color;
+                currentbtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentbtn.IconColor = color;
+                
+                currentbtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentbtn.ImageAlign = ContentAlignment.MiddleRight;
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0,currentbtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+        private void DisableButton()
+        {
+            if (currentbtn != null)
+            {
+                currentbtn.BackColor = Color.FromArgb(31,30,68);
+                currentbtn.ForeColor = Color.Gainsboro;
+                currentbtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentbtn.IconColor = Color.Gainsboro;
+                currentbtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentbtn.ImageAlign = ContentAlignment.MiddleLeft;
+                currentbtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+            
+
+            }
+        }
+
+        private void Page_Home_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void Page_2_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void Page_3_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+        }
+
+        private void Page_4_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color4);
+        }
+
+        private void Page_5_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color5);
+        }
+
+        private void Page_6_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color6);
+        }
+    }
+}
